d1248139007c10f4bca1449c1e7bbcef103cf3cf
william <124389603+william-simard@users.noreply.github.com>
2023-03-27 17:11:55 +0000

    DONE

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const base = parseInt(document.getElementById("base").value)
+  const hight = parseInt(document.getElementById("hight").value)
+
+  // calculate
+  const area = (base * hight) / 2
+  // output
+  document.getElementById("result").innerHTML =
+    "The area of the triangle is " + area + "cm²"
+}
